9b7a660e5a63967a3e850c8a9f4b63490e19365f
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-03-29 11:45:11 -0300

    corrigindo um erro
    corrigindo um erro na def homecursos

--- curso/views.py
+++ curso/views.py
@@ -10,4 +10,4 @@ def homecursos(request):
     context = {}
     lista_cursos = Curso.objects.all()
     context['lista_cursos'] = lista_cursos
-    return render(request, 'homecursos.html')
+    return render(request, 'homecursos.html', context)
