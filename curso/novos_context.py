d995e3b07347f5835191bcb4213e6906b09114bf
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-02 18:04:50 -0300

    gerenciador de contexto
    criei um gerenciador de contexto para o templete homecursos. Onde vamos ter a lista de novos cursos e cursos em alta.

---
+++ curso/novos_context.py
@@ -0,0 +1,10 @@
+from .models import Curso
+
+def lista_cursos_recentes(request):
+    lista_cursos = Curso.objects.all().order_by('-data_criacao')[0:10]
+    return {'lista_cursos_recentes': lista_cursos}
+
+
+def lista_cursos_emalta(request):
+    lista_cursos = Curso.objects.all().order_by('-visualizacoes')[0:10]
+    return {'lista_cursos_emalta': lista_cursos}
