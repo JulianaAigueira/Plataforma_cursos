e73b4ef88685fc95fbc74083f80463e8ed3c3cc9
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-03 15:02:55 -0300

    edições dinâmicas
    utilizei o JS para tornar minha barra de navegação dinâmica. E também coloquei o curso novo em destaque.

--- curso/novos_context.py
+++ curso/novos_context.py
@@ -2,9 +2,12 @@ from .models import Curso
 
 def lista_cursos_recentes(request):
     lista_cursos = Curso.objects.all().order_by('-data_criacao')[0:8]
-    return {'lista_cursos_recentes': lista_cursos}
+    curso_destaque = lista_cursos[0]
+    return {'lista_cursos_recentes': lista_cursos, 'curso_destaque': curso_destaque}
 
 
 def lista_cursos_emalta(request):
     lista_cursos = Curso.objects.all().order_by('-visualizacoes')[0:8]
     return {'lista_cursos_emalta': lista_cursos}
+
+
