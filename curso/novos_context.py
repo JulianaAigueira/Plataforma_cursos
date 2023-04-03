d03fda0a722c0778ddae4a01dfad158c5e9d40a6
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-03 11:32:51 -0300

    editando página dos cursos
    editando a página homecursos colocando os cursos novos e os altas tudo no html5 e no css para dar uma dinâmica no site

--- curso/novos_context.py
+++ curso/novos_context.py
@@ -1,10 +1,10 @@
 from .models import Curso
 
 def lista_cursos_recentes(request):
-    lista_cursos = Curso.objects.all().order_by('-data_criacao')[0:10]
+    lista_cursos = Curso.objects.all().order_by('-data_criacao')[0:8]
     return {'lista_cursos_recentes': lista_cursos}
 
 
 def lista_cursos_emalta(request):
-    lista_cursos = Curso.objects.all().order_by('-visualizacoes')[0:10]
+    lista_cursos = Curso.objects.all().order_by('-visualizacoes')[0:8]
     return {'lista_cursos_emalta': lista_cursos}
