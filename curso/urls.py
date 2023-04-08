4a27f9a633caf7b64f28640a68d61be8a9d8cc26
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-04-08 10:45:41 -0300

    tamplete editar perfil
    feito o tamplete editarperfil e criando o editar nova senha

--- curso/urls.py
+++ curso/urls.py
@@ -1,6 +1,6 @@
 # url - view - template
 
-from django.urls import path, include
+from django.urls import path, reverse_lazy
 from .views import Homecursos, Homepage, Detalhescurso, Pesquisacurso, Paginaperfil, Criarconta
 from django.contrib.auth import views as auth_view
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path('pesquisa/', Pesquisacurso.as_view(), name='pesquisacurso'),
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
+    path('editarperfil/<int:pk>', Paginaperfil.as_view(), name='editarperfil'),
     path('criarconta/', Criarconta.as_view(), name='criarconta'),
+    path('mudarsenha/',auth_view.PasswordChangeView.as_view(template_name='editarperfil.html', success_url=reverse_lazy('curso:homecursos')), name='mudarsenha'),
 ]
--- curso/views.py
+++ curso/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect, reverse
 from .models import Curso, Usuario
 from .forms import CriarContaForm, FormHomepage
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -84,8 +84,13 @@ class Pesquisacurso(LoginRequiredMixin, ListView):
         else:
             return None
 
-class Paginaperfil(LoginRequiredMixin, TemplateView):
+class Paginaperfil(LoginRequiredMixin, UpdateView):
     template_name = 'editarperfil.html'
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('curso:homecursos')
 
 
 class Criarconta(FormView):
